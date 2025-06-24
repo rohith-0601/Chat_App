6bbe9973beaafa942c5e35eb16ef658cb09ddfc3
rohith <rohithperugu3@gmail.com>
2025-06-24 23:47:01 +0530

    changed url

--- client/src/Chatpages/Chat.jsx
+++ client/src/Chatpages/Chat.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import io from "socket.io-client";
-const socket = io("http://localhost:5001");
+const socket = io("https://chat-app-jscd.onrender.com");
 
 function Chatpage() {
   const { state } = useLocation();
